73e3511be6ae863672b9456fc3d40561e96f3406
ShivaaZr <shivazarei33@gmail.com>
2025-07-15 00:53:51 +0330

    Update articles

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from blog.models import Article
 
 
 def home(request):
+    articles = Article.objects.all()
     
-    return render(request, "home\index.html", {})
+    return render(request, "home\index.html", {'articles':articles})
