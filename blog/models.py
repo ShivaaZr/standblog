8883f72c428bf6458c8f73c5dc0f44f440054454
ShivaaZr <shivazarei33@gmail.com>
2025-07-20 21:56:20 +0330

    Add Category and Profile models

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.title
 
 
 class Article(models.Model):
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    title = models.CharField(max_length=70)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
+    title = models.CharField(max_length=70, help_text="ENTER A VALID TITLE..", unique=True)
     body = models.TextField()
-    image = models.ImageField(upload_to="images/article")
+    image = models.ImageField(upload_to="images/articles")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+ 
     
     
     def __str__(self):
